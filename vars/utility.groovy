6582af7e007bf1edb8a3e1c47041f96e7b731de6
ekanshverma <ekansh.verma@tothenew.com>
2019-10-18 00:59:09 +0530

    adding createDockerRegistry func

--- vars/utility.groovy
+++ vars/utility.groovy
@@ -15,59 +15,56 @@ def getECRName(){
     String repo = steps.sh(returnStdout: true, script: "aws ecr get-login --region us-west-2 --no-include-email | cut -d \" \" -f 7" ).trim()
     return repo.replace("https://", "")
 }
-def createDockerRegistry() {
-    def sout = new StringBuilder(), serr = new StringBuilder()
-    command = "aws ecr describe-repositories --region us-west-2"
-    echo command
-    def proc = command.execute()
-    proc.consumeProcessOutput(sout, serr)
-    proc.waitForOrKill(1000)
-    echo "out> $sout "
-    echo "err> $serr"
-    if ("$sout"){
-        def repoMap = new JsonSlurper().parseText(sout)
-        repoMap.each
-        return "$sout"
-    }
-    else{
-        return "NoTagDefined"
-    }
-}
 def createDockerRegistry(def dockerRepo) {
     def sout = new StringBuilder(), serr = new StringBuilder()
     command = "aws ecr describe-repositories --region us-west-2"
     def proc = command.execute()
     proc.consumeProcessOutput(sout, serr)
     proc.waitForOrKill(10000)
-    if ("$sout"){
-        def str = "${sout}"
-        def parser = new JsonSlurper()
-        def json = parser.parseText(str)
-        def reg_found = false
-        assert json instanceof Map
-        json.repositories.repositoryName.each{ 
-        v -> if(v.toString().contains(dockerRepo)){
-            reg_found = true
-        }
-    }
-    if(reg_found == true){
-        println("Registry exists")
+    if ("$serr"){
+        return "Unable to find registry information error is ${serr}"
     }
     else{
-        println("Creating registry now ...")
-        def sout_rc = new StringBuilder(), serr_rc = new StringBuilder()
-        command = "aws ecr create-repository --repository-name ${dockerRepo} --region us-west-2"
-        def proc_rc = command.execute()
-        proc_rc.consumeProcessOutput(sout_rc, serr_rc)
-        proc.waitForOrKill(10000)
-        if(serr){
-            println("Error creating repository")
-            return
+        if("$sout"){
+            def str = "${sout}"
+            def parser = new JsonSlurper()
+            def json = parser.parseText(str)
+            def reg_found = false
+            assert json instanceof Map
+            json.repositories.repositoryName.each{ 
+                v -> if(v.toString().contains(dockerRepo)){
+                    reg_found = true
+                }
+            }
+            if(reg_found == true){
+                println("Registry exists")
+            }
+            else{
+                println("Creating registry now ...")
+                def sout_rc = new StringBuilder(), serr_rc = new StringBuilder()
+                command = "aws ecr create-repository --repository-name ${dockerRepo} --region us-west-2"
+                def proc_rc = command.execute()
+                proc_rc.consumeProcessOutput(sout_rc, serr_rc)
+                proc.waitForOrKill(10000)
+                if(serr){
+                    println("Error creating repository")
+                    return
+                }
+            }
         }
-    }
-    }
-    else{
-        return "Unable to find registry information"
+        else{
+            println("Creating registry now ...")
+            def sout_rc = new StringBuilder(), serr_rc = new StringBuilder()
+            command = "aws ecr create-repository --repository-name ${dockerRepo} --region us-west-2"
+            def proc_rc = command.execute()
+            proc_rc.consumeProcessOutput(sout_rc, serr_rc)
+            proc.waitForOrKill(10000)
+            if(serr){
+                println("Error creating repository")
+                return "${sout_rc} : ${$err_rc}"
+            }
+            return "${sout_rc} : ${serr_rc}"
+        }
     }
 }
 
